3ecde5c1a26cd1ecfb630cbe8449522e8df612c0
BotsOP <prutmaild@gmail.com>
2021-11-20 17:28:54 +0100

    Added enemy health bar and idle movement and new spaceship sprite

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour, IDamagable
 {
     public float maxHealth;
     private float health;
+    public Slider healthSlider;
+    private bool running;
+    private bool waiting;
+
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +23,38 @@ public class Enemy : MonoBehaviour, IDamagable
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(idle());
+        if (waiting)
+        {
+            transform.position += direction / 100;
+        }
+    }
+
+    private IEnumerator idle()
+    {
+        if (!running)
+        {
+            running = true;
+            waiting = false;
+            
+            direction = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5)).normalized;
+            
+            yield return new WaitForSeconds(2f);
+            waiting = true;
+            yield return new WaitForSeconds(2f);
+            running = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        throw new NotImplementedException();
     }
 
     public void TakeDamage(float damage)
     {
-        Debug.Log("taking dmg");
         health -= damage;
+        healthSlider.value = health / 100;
         if (health <= 0)
         {
             Debug.Log("Died");
